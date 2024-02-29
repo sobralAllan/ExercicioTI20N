1e74d9d11ee9816df256b6ef5fcbe42648b8bf89
ALLAN SOBRAL DA SILVA <allan.ssilva4@senacsp.edu.br>
2024-02-28 22:17:19 -0300

    Primeiro Commit

---
+++ ExerciciosTI20N/Control.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosTI20N
+{
+    class Control
+    {
+        public Control()
+        {
+
+        }//fim do construtor
+
+        public void Menu()
+        {
+            Console.WriteLine("\n\nEscolha uma das opções abaixo: \n" + 
+                              "0. Sair\n"                             +
+                              "1. Exercício 01\n"                     +
+                              "2. Exercício 02\n"                     +
+                              "3. Exercício 03\n"                     +
+                              "4. Exercício 04\n");
+        }//fim do menu
+
+    }//fim da classe
+}//fim do projeto
---
+++ ExerciciosTI20N/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosTI20N
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }//fim do main
+    }//fim da classe
+}//fim do projeto
